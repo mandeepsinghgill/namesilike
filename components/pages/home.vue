95c602118dbe6b977f98e7ce556f0475862301d0
MARK <mandeepgill@Mandeeps-MacBook-Pro.local>
2018-02-18 17:28:54 +0530

    Async Data

--- components/pages/home.vue
+++ components/pages/home.vue
@@ -3,8 +3,22 @@
     <h1>{{ $t('titles.main') }}</h1>
     {{ $t('home') }}
     <p>{{ $t('content.siteDescription') }}</p>
+    <h1>
+      {{ ip }}
+    </h1>
   </section>
 </template>
 <script>
-  export default {}
+  export default {
+    data() {
+      return {
+        ip: null
+      }
+    },
+    mounted() {
+      this.$axios.$get('http://icanhazip.com').then(response => {
+        this.ip = response.trim()
+      });
+    }
+  }
 </script>
